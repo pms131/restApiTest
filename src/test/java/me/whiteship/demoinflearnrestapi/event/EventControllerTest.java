9e0c34d3d47e263fe637124d53097d673bfb45e9
pms131 <sportssk@naver.com>
2020-08-26 08:51:05 +0900

    polishing

--- src/test/java/me/whiteship/demoinflearnrestapi/event/EventControllerTest.java
+++ src/test/java/me/whiteship/demoinflearnrestapi/event/EventControllerTest.java
@@ -2,6 +2,7 @@ package me.whiteship.demoinflearnrestapi.event;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -61,6 +63,8 @@ public class EventControllerTest {
 			   .andDo(print())
 			   .andExpect(status().isCreated())
 			   .andExpect(jsonPath("id").exists())
+			   .andExpect(header().exists(HttpHeaders.LOCATION))
+			   .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
 			   ;
 	}
 }
