908433875b5e2895d907eca71c702daf13129732
pms131 <sportssk@naver.com>
2020-08-28 13:48:19 +0900

    Body for BadRequest

--- src/main/java/me/whiteship/demoinflearnrestapi/event/EventController.java
+++ src/main/java/me/whiteship/demoinflearnrestapi/event/EventController.java
@@ -33,10 +33,14 @@ public class EventController {
 	public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
 		
 		if(errors.hasErrors()) {
-			return ResponseEntity.badRequest().build();
+			return ResponseEntity.badRequest().body(errors);
 		}
 		
 		eventValidator.validate(eventDto, errors);
+		
+		if(errors.hasErrors()) {
+			return ResponseEntity.badRequest().body(errors);
+		}
 		
 		Event event = modelMapper.map(eventDto, Event.class);
 		Event newEvent = this.eventRepository.save(event);
